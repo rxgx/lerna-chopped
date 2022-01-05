35d97387bf15b6379be3ce70e3f85f0dfe88ee83
Ryan Gasparini <ryan@rxgx.com>
2022-01-04 19:11:50 -0700

    chore: run both www and api in codespaces (#103)
    * chore: run both www and api in codespaces
    
    * remove compose from dev container
    
    * update episode data
    
    * update episodes
    
    * update dev container

--- packages/episodes/index.js
+++ packages/episodes/index.js
@@ -3,6 +3,7 @@ module.exports = [
     name: 'Octopus, Duck, Animal Crackers',
     numOverall: 1,
     numSeason: 1,
+    numEpisode: 1,
     date: 'January 13, 2009',
     ingredients: [
       { round: 'Appetizer', name: 'baby octopus' },
@@ -22,6 +23,7 @@ module.exports = [
     name: 'Tofu, Blueberries, Oysters',
     numOverall: 2,
     numSeason: 1,
+    numEpisode: 2,
     date: 'January 20, 2009',
     ingredients: [
       { round: 'Appetizer', name: 'firm tofu' },
@@ -42,6 +44,7 @@ module.exports = [
     name: 'A Prickly Situation',
     numOverall: 14,
     numSeason: 2,
+    numEpisode: 1,
     date: 'June 14, 2009',
     ingredients: [
       { round: 'Appetizer', name: 'brioche' },
@@ -61,6 +64,7 @@ module.exports = [
     name: 'Salty Veggies & Heated Battles',
     numOverall: 15,
     numSeason: 2,
+    numEpisode: 2,
     date: 'June 16, 2009',
     ingredients: [
       { round: 'Appetizer', name: 'sea beans', url: 'https://en.wikipedia.org/wiki/Glasswort' },
@@ -73,6 +77,83 @@ module.exports = [
       { round: 'Dessert', name: 'jicama' },
       { round: 'Dessert', name: 'pepitas' },
       { round: 'Dessert', name: 'blueberries' }
+    ]
+  },
+  {
+    name: 'High Hopes',
+    numOverall: 16,
+    numSeason: 2,
+    numEpisode: 3,
+    date: 'June 23, 2009',
+    ingredients: [
+      { round: 'Appetizer', name: 'calamari' },
+      { round: 'Appetizer', name: 'Brussels sprouts' },
+      { round: 'Appetizer', name: 'Asian pears' },
+      { round: 'Entrée', name: 'bison' },
+      { round: 'Entrée', name: 'dried mango' },
+      { round: 'Entrée', name: 'leeks' },
+      { round: 'Entrée', name: 'Belgian-style ale' },
+      { round: 'Dessert', name: 'ancho chiles' },
+      { round: 'Dessert', name: 'semi-sweet chocolate' },
+      { round: 'Dessert', name: 'black plums' }
+    ]
+  },
+  {
+    numOverall: 17,
+    numSeason: 2,
+    numEpisode: 4,
+    ingredients: [
+      { round: 'Appetizer', name: 'beef shoulder' },
+      { round: 'Appetizer', name: 'fish sauce' },
+      { round: 'Appetizer', name: 'canned pumpkin' },
+      { round: 'Entrée', name: 'flounder' },
+      { round: 'Entrée', name: 'yellow plantains' },
+      { round: 'Entrée', name: 'baby bok choy' },
+      { round: 'Entrée', name: 'mini watermelon' },
+      { round: 'Dessert', name: 'tomatillos' },
+      { round: 'Dessert', name: 'crème fraîche' },
+      { round: 'Dessert', name: 'plain donuts' }
+    ]
+  },
+  {
+    name: 'Chopped Champions Event, Round Three: All-Stars Showdown',
+    numOverall: 25,
+    numSeason: 2,
+    numEpisode: 12,
+    date: 'September 22, 2009',
+    ingredients: [
+      { round: 'Appetizer', name: 'lavash bread' },
+      { round: 'Appetizer', name: 'preserved lemons' },
+      { round: 'Appetizer', name: 'squab' },
+      { round: 'Entrée', name: 'bluefish' },
+      { round: 'Entrée', name: 'papaya' },
+      { round: 'Entrée', name: 'ginger snaps' },
+      { round: 'Entrée', name: 'beef jerky' },
+      { round: 'Dessert', name: 'butter crackers' },
+      { round: 'Dessert', name: 'grapefruit' },
+      { round: 'Dessert', name: 'feta cheese' },
+      { round: 'Dessert', name: 'gin' }
+    ]
+  },
+  {
+    name: '$50,000 Tournament: Grand Finale',
+    date: 'September 28, 2010',
+    numOverall: 60,
+    numSeason: 5,
+    numEpisode: 8,
+    ingredients: [
+      { round: 'Appetizer', name: 'geoduck' },
+      { round: 'Appetizer', name: "Buddha's hand" },
+      { round: 'Appetizer', name: 'black radishes' },
+      { round: 'Appetizer', name: 'waffle cones' },
+      { round: 'Entrée', name: 'smelt' },
+      { round: 'Entrée', name: 'dried red currants' },
+      { round: 'Entrée', name: 'cheese curls' },
+      { round: 'Entrée', name: 'rack of wild boar' },
+      { round: 'Dessert', name: 'champagne' },
+      { round: 'Dessert', name: 'dragon fruit' },
+      { round: 'Dessert', name: 'tahini' },
+      { round: 'Dessert', name: 'cake flour' }
     ]
   }
 ]
